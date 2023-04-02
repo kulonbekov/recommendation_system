209d6f5422af588bfb28baf9b765a7127a36b04a
Kulonbekov <kulonbekov@gmail.com>
2023-04-03 01:15:59 +0600

    add description to 'data.sql' file

--- recommendation_system/src/main/resources/data.sql
+++ recommendation_system/src/main/resources/data.sql
@@ -1,18 +1,31 @@
+/*Добавляется два роли (ROLE_USER, ROLE_ADMIN)*/
 insert into tb_role (id, created, status, updated, name) VALUES (1, '2023-03-29 02:27:35', 'ACTIVE', '2023-03-29 02:27:41', 'ROLE_USER');
 insert into tb_role (id, created, status, updated, name) VALUES (2, '2023-03-29 02:27:35', 'ACTIVE', '2023-03-29 02:27:41', 'ROLE_ADMIN');
 
+/*Пользователь 'kulonbekov' с ролем (ROLE_ADMIN)*/
 insert into tb_user(id,created,status,updated,age, email, gender, password, username)
 VALUES (1, '2023-03-29 02:28:38.593', 'ACTIVE', '2023-03-29 02:28:38.593', 29, 'kulonbekov@gmail.com','MALE', '$2a$04$TgiPX6SJgrjQ4RQhKNWOnOwJ.ohCgTZ63kxc9ysIcnhFol21wg9Y2', 'kulonbekov');
 
-insert into tb_user_roles(user_id,role_id)VALUES (1,2);
+/*Пользователь 'adil' с ролем (ROLE_USER)*/
+insert into tb_user(id,created,status,updated,age, email, gender, password, username)
+VALUES (2, '2023-03-29 02:28:38.593', 'ACTIVE', '2023-03-29 02:28:38.593', 25, 'sehami4053@djpich.com','MALE', '$2a$10$DXlADomsliqPm44E43IS0O34vSqpBD2xHIUAhf0gPnbC3vL4bTGXy', 'adil');
 
+/*В промежуточной таблице добавляются роли к пользователям*/
+insert into tb_user_roles(user_id,role_id)VALUES (1,2);
+insert into tb_user_roles(user_id,role_id)VALUES (2,1);
+
+/*В таблицу добавляется жанры к треку*/
 insert into tb_genre(id,created,status,updated,name)VALUES (1,'2023-03-29 02:28:38.593','ACTIVE', '2023-03-29 02:28:38.593','Классическая музыка');
 insert into tb_genre(id,created,status,updated,name)VALUES (2,'2023-03-29 02:28:38.593','ACTIVE', '2023-03-29 02:28:38.593','Hip Hop');
 insert into tb_genre(id,created,status,updated,name)VALUES (3,'2023-03-29 02:28:38.593','ACTIVE', '2023-03-29 02:28:38.593','Индийская поп-музыка');
 insert into tb_genre(id,created,status,updated,name)VALUES (4,'2023-03-29 02:28:38.593','ACTIVE', '2023-03-29 02:28:38.593','Rap');
 insert into tb_genre(id,created,status,updated,name)VALUES (5,'2023-03-29 02:28:38.593','ACTIVE', '2023-03-29 02:28:38.593','Поп-музыка');
 
+/*В этом запросе добавляется предпочитаемые жанры пользователя 'adil'*/
+insert into tb_user_genres(user_id,genre_id)VALUES (2,1);
+insert into tb_user_genres(user_id,genre_id)VALUES (2,4);
 
+/*Добавляется контент в таблицу 'tb_music' */
 insert into tb_music (id,created,status,updated,album,author,date_of_issue,image,name,number_of_plays, song)
 VALUES (1,'2023-04-01 20:28:22.02','ACTIVE','2023-04-01 20:28:22.02','Мурас', 'Мирбек Атабеков', 2017, 'C:\Users\Comp\IdeaProjects\Bootcamp_12\recommendation_system\..\recommendation_system\recommendation_system\src\main\resources\musics\images\muras.jpg', 'Мурас', 500, 'C:\Users\Comp\IdeaProjects\Bootcamp_12\recommendation_system\..\recommendation_system\recommendation_system\src\main\resources\musics\songs\mirbek-atabekov-muras.mp3');
 insert into tb_music (id,created,status,updated,album,author,date_of_issue,image,name,number_of_plays, song)
@@ -24,7 +37,7 @@ VALUES (4,'2023-04-01 20:28:22.02','ACTIVE','2023-04-01 20:28:22.02','Ауылы
 insert into tb_music (id,created,status,updated,album,author,date_of_issue,image,name,number_of_plays, song)
 VALUES (5,'2023-04-01 20:28:22.02','ACTIVE','2023-04-01 20:28:22.02','Айланып учуп кетем', 'FREEMAN 996', 2020, 'C:\Users\Comp\IdeaProjects\Bootcamp_12\recommendation_system\..\recommendation_system\recommendation_system\src\main\resources\musics\images\айланып-учуп-кетем.jpg', 'Айланып учуп кетем', 490, 'C:\Users\Comp\IdeaProjects\Bootcamp_12\recommendation_system\..\recommendation_system\recommendation_system\src\main\resources\musics\songs\freeman-996-ajlanyp-uchup-ketem.mp3');
 
-
+/*По каждому треку добавляется жанры */
 insert into tb_music_genre(music_id,genre_id)VALUES (1,1);
 insert into tb_music_genre(music_id,genre_id)VALUES (1,3);
 insert into tb_music_genre(music_id,genre_id)VALUES (2,2);
